aedefa6ec8ce21accb28e7d5a0532eadf4fdbc56
Andrey <Andrey@MSI>
2023-04-30 18:47:48 -0300

    Commit counter1

--- src/main/java/com/mycompany/testguialibro/TestGuiaLibro.java
+++ src/main/java/com/mycompany/testguialibro/TestGuiaLibro.java
@@ -9,8 +9,29 @@ package com.mycompany.testguialibro;
  * @author Andrey
  */
 public class TestGuiaLibro {
-
+int counter = 0;
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        int count 0;
+        TestGuiaLibro [] m4a new TestGuiaLibro[20];
+        
+        int x =0;
+        
+        
+        
+        
+        
     }
+    
+    public int maybeNew(int index){
+        if (x<9) {
+         TestGuiaLibro m4 new TestGuiaLibro();
+         m4.counter = m4.counter +1;
+         return1
+        }
+        return 0;
+    }
+    
+    
 }
