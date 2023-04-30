a82998a981ab46a2786b6893c6ad1416fbce838b
Andrey <Andrey@MSI>
2023-04-30 17:31:08 -0300

    FirstCommit

---
+++ com.mycompany_ArrayListGuia9_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/arraylistguia9/ArrayListGuia9.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arraylistguia9;
+
+import entidades.Perro;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Andrey
+ */
+public class ArrayListGuia9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        ArrayList<Perro> prueba = new ArrayList<>();
+    }
+}
---
+++ com.mycompany_ArrayListGuia9_jar_1.0-SNAPSHOT/src/main/java/entidades/Perro.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author Andrey
+ */
+public class Perro {
+    private String raza;
+
+    public Perro() {
+    }
+
+    public Perro(String raza) {
+        this.raza = raza;
+    }
+    
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    @Override
+    public String toString() {
+        return "Perro{" + "raza=" + raza + '}';
+    }
+    
+    
+}
