2b730dda88ecf93dbeb76509b45dcd96004d9084
Andrey Rashkevych <polako.andrey@gmail.com>
2023-05-01 14:35:46 -0300

    01052023v3

--- com.mycompany_ArrayListGuia9_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/arraylistguia9/ArrayListGuia9.java
+++ com.mycompany_ArrayListGuia9_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/arraylistguia9/ArrayListGuia9.java
@@ -18,9 +18,16 @@ public class ArrayListGuia9 {
   
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
         PerroServ sc = new PerroServ();
+        ArrayList<Perro> arPerro =new ArrayList<>();
+        arPerro.add(sc.crearPerro());
+        for (int i = 0; i < 4; i++) {
+            arPerro.add(sc.crearPerro());
+        }
+      
 //        String[] pruba2 = {"Pepa", "pipa", "pupa"};
 //        String[] pruba4 = {"qepa", "qipa", "qupa"};
-//        ArrayList<String> prueba = new ArrayList<>(Arrays.asList(pruba2));
+        
+       
 //        ArrayList<String> prueba5 = new ArrayList<>();
 //        Iterator<String> it = prueba5.iterator();
 //
@@ -62,23 +69,29 @@ public class ArrayListGuia9 {
         
         sc.fabricarPerro(10);
         sc.mostrarPerro();
-        System.out.println("--------------");
-        sc.eliminarPerro(0);
-        sc.mostrarPerro();
-        System.out.println("--------------");
+//        System.out.println("--------------");
+//        sc.eliminarPerro(0);
+//        sc.mostrarPerro();
+//        System.out.println("--------------");
+//        
+//        sc.agregarPerro2(1);
+//        sc.mostrarPerro();
+//        System.out.println("--------------");
+//        System.out.println("Ingrese la raza que desea buscar y eliminar");
+//        sc.buscarElimanar2(leer.next());
+//          
+//         sc.mostrarPerro();
+//        
+//        System.out.println("--------------");
+//        sc.actualizarRaza("dog1", "Polako");
+//        sc.mostrarPerro();
         
-        sc.agregarPerro2(1);
-        sc.mostrarPerro();
-        System.out.println("--------------");
-        System.out.println("Ingrese la raza que desea buscar y eliminar");
-        sc.buscarElimanar2(leer.next());
-          
-         sc.mostrarPerro();
+      
         
-        System.out.println("--------------");
-        sc.actualizarRaza("dog1", "Polako");
-        sc.mostrarPerro();
-
+        for (Perro aux : arPerro) {
+            System.out.println(aux+"VErga");
+        }
+        
 
         
     }
