c42f9adc30fdf4809b78df43294aa0461f51d9c9
Andrey Rashkevych <polako.andrey@gmail.com>
2023-05-02 19:57:41 -0300

    02052023v1 --ej5

---
+++ PaisesAPP/src/main/java/com/mycompany/paisesapp/PaisesAPP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.paisesapp;
+
+/**
+ *
+ * @author Polako
+ */
+public class PaisesAPP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
