1754bae927d972e37482a4cd61c1b1610c42f7fd
Andrey Rashkevych <polako.andrey@gmail.com>
2023-05-01 21:16:33 -0300

    01052023v5 --ej4

--- src/main/java/com/mycompany/peliculasapp/PeliculasAPP.java
+++ src/main/java/com/mycompany/peliculasapp/PeliculasAPP.java
@@ -4,9 +4,12 @@
 
 package com.mycompany.peliculasapp;
 
+import entidades.Pelicula;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import servicios.PeliculaServ;
+import utilidades.Comparadores;
 
 /**
  *
@@ -16,12 +19,49 @@ public class PeliculasAPP {
 
     public static void main(String[] args) {
         PeliculaServ sc = new PeliculaServ();
-        List<PeliculaServ> listaPelic = new ArrayList();
+        List<Pelicula> listaPelic = sc.getListaPelis();
+        
+        
+        
         
         sc.fabricarPelis(5);
         sc.mostrarPeli2();
+        System.out.println("---------------------------------");
+        System.out.println("---------------------------------");
+        sc.mostrarPeliHora(30);
         
+        Collections.sort(listaPelic, Comparadores.ordenarPorHoraAsc);
         
+        for (Pelicula aux : listaPelic) {
+            System.out.println(aux.toString());
+        }
+        System.out.println("--------------acend-------------------");
+        System.out.println("---------------------------------");
+        sc.mostrarPeli2();
         
+        Collections.sort(listaPelic, Comparadores.ordenarPorHoraDesc);
+        
+        for (Pelicula aux : listaPelic) {
+            System.out.println(aux.toString());
+        }
+        System.out.println("---------------decend------------------");
+        System.out.println("---------------------------------");
+        sc.mostrarPeli2();
+        Collections.sort(listaPelic, Comparadores.ordenarPorDirector);
+        
+        for (Pelicula aux : listaPelic) {
+            System.out.println(aux.toString());
+        }
+        System.out.println("---------------Director------------------");
+        System.out.println("---------------------------------");
+        sc.mostrarPeli2();
+        Collections.sort(listaPelic, Comparadores.ordenarPorNombre);
+        
+        for (Pelicula aux : listaPelic) {
+            System.out.println(aux.toString());
+        }
+        System.out.println("---------------Pelicula------------------");
+        System.out.println("---------------------------------");
+        sc.mostrarPeli2();
     }
 }
